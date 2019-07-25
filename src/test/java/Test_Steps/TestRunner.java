de0a16beb4c48ec2fdaba4cd0b473d9b988fcd46
vigneshselvam1112 <53177775+vigneshselvam1112@users.noreply.github.com>
2019-07-25 16:33:08 +0530

    Update TestRunner.java

--- src/test/java/Test_Steps/TestRunner.java
+++ src/test/java/Test_Steps/TestRunner.java
@@ -6,7 +6,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="src/test/resources/Test/Demo.feature",plugin="json:target\\jsonreport.jason")
+@CucumberOptions(features="src/test/resources/Test/Demo.feature",plugin="json:target/jsonreport.jason")
 public class TestRunner {
 
 }
